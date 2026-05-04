b43cf1cfc42b79821f09d1914cbeb56071c5372b
Dev <157894292+deb-ian@users.noreply.github.com>
2026-05-04 20:41:57 +0530

    Went back to chromium lol

--- modules/home-manager/home.nix
+++ modules/home-manager/home.nix
@@ -26,7 +26,7 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
-    pkgs.vivaldi
+    pkgs.chromium
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
